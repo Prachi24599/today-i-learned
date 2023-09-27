729b82875abdb19dff49a9403aa3840766d2e4e8
Prachi24599 <prachi24599@gmail.com>
2023-09-27 23:16:56 +0530

    dom manipulation

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ console.log("Hello World");
 
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
+
+console.dir(btn);
 
 btn.addEventListener("click", function () {
+  factsList.innerHTML = "";
+
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
     btn.textContent = "Close";
