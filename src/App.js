90f994c7708abad920f2b5bd7eda1d92111d7bac
Prachi24599 <prachi24599@gmail.com>
2023-09-29 23:35:25 +0530

    divided code into components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./style.css";
+import Header from "./components/Header";
+import CategoryFilter from "./components/CategoryFilter";
 
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="Today I Learned Logo" />
-        <h1>Today I Learned!</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+    <>
+      <Header />
+      <CategoryFilter />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Today I Learned Logo" />
+        <h1>Today I Learned!</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+};
+
+export default Header;
