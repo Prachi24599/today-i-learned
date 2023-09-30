3a430f1612f2f49c26ccdc9262f82039cf5fb824
Prachi24599 <prachi24599@gmail.com>
2023-10-01 00:11:54 +0530

    displaying data after spinner

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import FactList from "./components/FactList";
 import NewFactForm from "./components/NewFactForm";
 import { useEffect, useState } from "react";
 import supabase from "./supabase";
+import Spinner from "./components/Spinner";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getFacts() {
-      const { data: facts, error } = await supabase.from("facts").select("*");
+      setIsLoading(true);
+      const { data: facts, error } = await supabase
+        .from("facts")
+        .select("*")
+        .limit(100);
       // console.log(facts);
       setFacts(facts);
+      setIsLoading(false);
     }
     getFacts();
   }, []);
@@ -27,7 +34,7 @@ function App() {
       )}
       <main className="main">
         <CategoryFilter />
-        <FactList facts={facts} />
+        {isLoading ? <Spinner /> : <FactList facts={facts} />}
       </main>
     </>
   );
