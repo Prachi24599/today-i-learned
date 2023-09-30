e1963dfc53571bb54a15c28ffeab74dc2ce2cbee
Prachi24599 <prachi24599@gmail.com>
2023-09-30 13:07:52 +0530

    handling form state

--- src/components/NewFactForm.js
+++ src/components/NewFactForm.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
 
 const NewFactForm = () => {
-  return <div className="fact-form">NewFactForm</div>;
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text, source, category);
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        type="text"
+        placeholder="Trustworthy Source..."
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose Category :</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name}>{cat.name.toUpperCase()}</option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
 };
 
 export default NewFactForm;
