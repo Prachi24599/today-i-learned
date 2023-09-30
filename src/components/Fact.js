6922f70cb00f9d8d473283a3ac46f9f1ca5479c7
Prachi24599 <prachi24599@gmail.com>
2023-10-01 02:40:45 +0530

    removed redundancy

--- src/components/Fact.js
+++ src/components/Fact.js
@@ -1,16 +1,7 @@
 import React, { useState } from "react";
 import supabase from "../supabase";
+import { CATEGORIES } from "../data";
 
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
 const Fact = ({ fact, setFacts }) => {
   const [isUpdating, setIsUpdating] = useState(false);
 
